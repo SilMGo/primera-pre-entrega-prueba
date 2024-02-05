d1e2bdb45d806f3d3ecddc7c69486431bb1e8b25
SilMGo <silvinamgonzalez0304@gmail.com>
2024-02-04 23:10:07 -0300

    proyecto con tabla-form-pipe-directivas-bootstrap

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,17 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FullNamePipe } from './full-name.pipe';
 import { ResaltadoDirective } from './resaltado.directive';
+import { RepetirDirective } from './repetir.directive';
 
 
 
 @NgModule({
   declarations: [
     FullNamePipe,
-    ResaltadoDirective
+    ResaltadoDirective,
+    RepetirDirective
   ],
   imports: [
     CommonModule
   ],
-  exports: [FullNamePipe, ResaltadoDirective]
+  exports: [FullNamePipe, ResaltadoDirective, RepetirDirective]
 })
 export class SharedModule { }
